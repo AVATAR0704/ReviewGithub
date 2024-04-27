1ba36cf7b7945eeee125388dddb5b2e2bbff4fe6
Samandar <samandartoshpulatov0704@gmail.com>
2024-04-27 22:07:28 +0500

    development2

--- ReviewGithub/ContentView.swift
+++ ReviewGithub/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
                 .fontWeight(.black)
             Rectangle()
                 .cornerRadius(10)
+            Circle()
         }
         .padding()
         .foregroundColor(.purple)
