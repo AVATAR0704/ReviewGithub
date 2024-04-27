1f0086508e07abaad20af72e3877308c8fdde1af
Samandar <samandartoshpulatov0704@gmail.com>
2024-04-27 21:33:29 +0500

    Initial Commit

---
+++ ReviewGithub/ReviewGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewGithubApp.swift
+//  ReviewGithub
+//
+//  Created by Samandar on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ReviewGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
